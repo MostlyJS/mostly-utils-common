b4098983d3dd7126e97cf741851257e7aa26a481
forafish <fora.luo@gmail.com>
2018-03-01 14:46:50 +0800

    add nextFibonacci function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 if (!global._babelPolyfill) { require('babel-polyfill'); }
 
 module.exports.cacheMap = require('./lib/cache-map');
+module.exports.nextFibonacci = require('./lib/next-fibonacci');
---
+++ src/next-fibonacci.js
@@ -0,0 +1,17 @@
+import fp from 'mostly-func';
+
+function *fibonacci(n) {
+  const infinite = !n && n !== 0;
+  let current = 0;
+  let next = 1;
+  
+  while (infinite || n--) {
+    yield current;
+    [current, next] = [next, current + next];
+  }
+}
+
+export default function nextFibonacci(current) {
+  const [...fibs] = fibonacci(100);
+  return fp.reduce((next, val) => current < val? fp.reduced(val) : next, 1, fibDays);
+}
