0d07f8a6af7e90c87f045c801ffb99774f2135ff
forafish <fora.luo@gmail.com>
2018-03-14 18:23:00 +0800

    add default cacheMap options

--- src/cache-map.js
+++ src/cache-map.js
@@ -4,5 +4,9 @@ LruCache.prototype.delete = LruCache.prototype.del;
 LruCache.prototype.clear = LruCache.prototype.reset;
 
 export default function cacheMap (options) {
+  options = Object.assign({}, options, {
+    max: 200,
+    maxAge: 1000 * 60 * 10
+  });
   return new LruCache(options);
 }
