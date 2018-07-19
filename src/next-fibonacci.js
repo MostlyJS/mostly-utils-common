5c1dfe8620d4a8af93ae51165e9ec0f033088918
forafish <fora.luo@gmail.com>
2018-07-19 16:28:48 +0800

    lint empty lines

--- src/next-fibonacci.js
+++ src/next-fibonacci.js
@@ -4,7 +4,7 @@ function *fibonacci (n) {
   const infinite = !n && n !== 0;
   let current = 0;
   let next = 1;
-  
+
   while (infinite || n--) {
     yield current;
     [current, next] = [next, current + next];
